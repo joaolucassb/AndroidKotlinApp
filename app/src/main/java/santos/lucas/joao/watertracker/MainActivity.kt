0cbdcd4f821811a5845c35e287f3332b02b42b0a
João Lucas <97396678+joaolucassb@users.noreply.github.com>
2024-10-08 18:01:35 -0300

    criado layout da MainActivity

--- app/src/main/java/santos/lucas/joao/watertracker/MainActivity.kt
+++ app/src/main/java/santos/lucas/joao/watertracker/MainActivity.kt
@@ -2,12 +2,19 @@ package santos.lucas.joao.watertracker
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import santos.lucas.joao.watertracker.databinding.ActivityMainBinding
+import santos.lucas.joao.watertracker.sqlite.WaterEntryController
 
+class MainActivity : ComponentActivity()
 
-class MainActivity : ComponentActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var consumptionController: WaterEntryController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        //TODO
     }
 }
 
